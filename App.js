a6ef96b5384a58304d85a89ce4931dec08994e1d
nympheastudio <perodo@gmail.com>
2022-11-13 23:14:36 +0100

    test

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+//import MapView, {UrlTile,  Marker } from 'react-native-maps';
+import MapView from 'react-native-map-markerclustering';
+import {UrlTile,  Marker } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions,Platform } from 'react-native';
+import * as FileSystem from 'expo-file-system';
 
 export default function App() {
+
+  const [urlTemplate, setUrlTemplate] = useState('https://a.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png');
+  const [region, setRegion]= useState({
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+    const [mapRegion, setmapRegion] = useState({
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+  
+    function renderRandomMarkers(n) {
+      const { latitude, longitude, latitudeDelta, longitudeDelta } = mapRegion;
+      return new Array(n).fill().map((x, i) => (
+        <Marker
+          key={i}
+          coordinate={{
+            latitude: latitude + (Math.random() - 0.5) * latitudeDelta,
+            longitude: longitude + (Math.random() - 0.5) * longitudeDelta
+          }}
+        />
+      ));
+    }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} 
+    //  mapType={Platform.OS == "android" ? "none" : "standard"}
+      region={region}
+     // onRegionChange={setRegion(region)}
+    
+      >
+        <Marker coordinate={mapRegion} title='Marker' />
+        {renderRandomMarkers(14)}
+     <UrlTile
+            urlTemplate={urlTemplate}
+           //maximumZ={19}
+             tileCachePath={`${FileSystem.documentDirectory}tiles/5dem_grid/`}
+          /></MapView>
     </View>
   );
 }
@@ -16,5 +60,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
